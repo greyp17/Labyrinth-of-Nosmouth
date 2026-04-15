3dc4a636566e14e967078fb58c6ea84255c6bfe8
greyp17 <grey.patton239@gmail.com>
2026-04-15 01:13:11 -0400

    framework for the you win text in main branch

--- MyForm.h
+++ MyForm.h
@@ -10,7 +10,7 @@ namespace GUILabyrinthProject {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	Maze* nativeMaze;
+	
 	/// <summary>
 	/// Summary for MyForm
 	/// </summary>
@@ -47,9 +47,11 @@ namespace GUILabyrinthProject {
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::PictureBox^ MazeBox1;
 
+	private: Maze* nativeMaze;
 
 	private: int playerCol;
 	private: int playerRow;
+	private: System::Windows::Forms::Label^ label1;
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -69,6 +71,7 @@ namespace GUILabyrinthProject {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->MazeBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->MazeBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -84,6 +87,7 @@ namespace GUILabyrinthProject {
 			// 
 			// button1
 			// 
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
 			this->button1->Location = System::Drawing::Point(13, 530);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(42, 21);
@@ -91,31 +95,27 @@ namespace GUILabyrinthProject {
 			this->button1->Text = L"Easy";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(
-				System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left);
 			// 
 			// button2
 			// 
+			this->button2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
 			this->button2->Location = System::Drawing::Point(61, 530);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(55, 20);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Medium";
 			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(
-				System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left);
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_click);
 			// 
 			// button3
 			// 
+			this->button3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
 			this->button3->Location = System::Drawing::Point(122, 531);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(48, 19);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"Hard";
 			this->button3->UseVisualStyleBackColor = true;
-			this->button3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(
-				System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left);
 			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_click);
 			// 
 			// MazeBox1
@@ -124,35 +124,38 @@ namespace GUILabyrinthProject {
 			this->MazeBox1->Location = System::Drawing::Point(18, 16);
 			this->MazeBox1->Name = L"MazeBox1";
 			this->MazeBox1->Size = System::Drawing::Size(450, 450);
+			this->MazeBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
 			this->MazeBox1->TabIndex = 3;
 			this->MazeBox1->TabStop = false;
 			this->MazeBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::MazeBox1_Paint);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
-			this->MazeBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
-			this->MazeBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(
-				System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(23, 470);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(113, 13);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"Labryinth of Nosmouth";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(670, 556);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->MazeBox1);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->KeyPreview = true;
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->MazeBox1))->EndInit();
-			this->ResumeLayout(false);
-
-			//keys
-			this->KeyPreview = true;
 			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &MyForm::MyForm_KeyDown);
-			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->MazeBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -250,6 +253,15 @@ namespace GUILabyrinthProject {
 		nativeMaze->setMapArrayValue(newRow, newCol, 2); // Move player
 		playerRow = newRow;
 		playerCol = newCol;
+
+		// Check for bottom-right corner (win condition)
+		int rows = nativeMaze->getMazeHeight();
+		int cols = nativeMaze->getMazeWidth();
+		if (rows > 0 && cols > 0 && playerRow == rows - 1 && playerCol == cols - 1) {
+			System::Windows::Forms::MessageBox::Show("You win", "win", System::Windows::Forms::MessageBoxButtons::OK, System::Windows::Forms::MessageBoxIcon::Information);
+			this->MazeBox1->Invalidate();
+			return;
+		}
 
 		nativeMaze->MoveEnemies();
 		this->MazeBox1->Invalidate();
